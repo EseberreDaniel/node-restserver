e867046c6680dfc20d3c3beb73802e0b2fb96703
Juan Daniel Eseberre Torres <14170899@itculiacan.edu.mx>
2019-02-11 12:28:23 -0700

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://danieleseberre:hyjo9696@cluster0-shard-00-00-lcm6p.mongodb.net:27017,cluster0-shard-00-01-lcm6p.mongodb.net:27017,cluster0-shard-00-02-lcm6p.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+    urlDB = process.env.DB_URI;
 }
 
 process.env.URLDB = urlDB;
