f0de2b84098a65f3096ec028322b39e0f8c66aed
Juan Daniel Eseberre Torres <14170899@itculiacan.edu.mx>
2019-02-11 13:02:31 -0700

    Prueba en heroku

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     console.log(process.env.DB_URI);
-    urlDB = process.env.DB_URI;
+    urlDB = 'mongodb://danieleseberre:hyjo9696@cluster0-shard-00-00-lcm6p.mongodb.net:27017,cluster0-shard-00-01-lcm6p.mongodb.net:27017,cluster0-shard-00-02-lcm6p.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
 }
 
 process.env.URLDB = urlDB;
