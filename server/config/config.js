0ec7e630418b8a9da23b31a5fee495ccce17192a
Juan Daniel Eseberre Torres <14170899@itculiacan.edu.mx>
2019-02-13 08:28:36 -0700

    SEED

--- server/config/config.js
+++ server/config/config.js
@@ -7,6 +7,20 @@ process.env.PORT = process.env.PORT || 3000;
 
 
 //=====
+//Vencimiento del Token
+//======
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24;
+
+
+
+
+//=====
+//SEED de autenticacion
+//======
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarollo';
+
+
+//=====
 //Entorno
 //======
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
@@ -15,8 +29,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    console.log(process.env.DB_URI);
-    urlDB = 'mongodb://danieleseberre:hyjo9696@cluster0-shard-00-00-lcm6p.mongodb.net:27017,cluster0-shard-00-01-lcm6p.mongodb.net:27017,cluster0-shard-00-02-lcm6p.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+    urlDB = process.env.DB_URI;
 }
 
 process.env.URLDB = urlDB;
